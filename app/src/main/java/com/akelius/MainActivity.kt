60b74e291d04dd003e6d0b831f132fc5fd37134c
Noshair Amjad <noshairamjad7@gmail.com>
2022-09-10 13:03:36 +0200

    Initial commit

---
+++ app/src/main/java/com/akelius/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.akelius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
